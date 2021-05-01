50d4cd08ee67391b8aac9642281ca65249bd1c17
Alexandre Gressier <alexandre@gressier.dev>
2021-05-01 22:43:56 +0200

    Showcase the use of state with a button

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,14 @@ class App extends Component<{}, AppState> {
         }
     }
 
+    changeStateData = () => {
+        this.setState({ username: this.state.username === "Alex" ? "Johnny" : "Alex"})
+    }
+
     render = () => (
         <div>
-            <h4 className="bg-primary text-white text-center p-2">{this.state.username}'s To-do List</h4>
+            <h4 className="bg-primary text-white text-center p-2">{ this.state.username }'s To-do List</h4>
+            <button className="btn btn-primary m-2" onClick={ this.changeStateData }>Change</button>
         </div>
     )
 }
