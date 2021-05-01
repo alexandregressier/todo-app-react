1fbed69bc623ce14b93ff12cc45c1d5604d5c495
Alexandre Gressier <alexandre@gressier.dev>
2021-05-01 23:46:01 +0200

    Implement the listing of items to be done

--- src/App.tsx
+++ src/App.tsx
@@ -27,22 +27,48 @@ export default class App extends Component<{}, AppState> {
         if (!this.state.todoItems.find(x => x.action === this.state.newItemText)) {
             this.setState({
                 todoItems: [...this.state.todoItems, { action: this.state.newItemText, done: false }],
-                newItemText: ""
+                newItemText: "",
             })
         }
     }
+
+    todoTableRows = () =>
+        this.state.todoItems.map(item =>
+            <tr key={ item.action }>
+                <td>{ item.action }</td>
+                <td><input type="checkbox" checked={ item.done } onChange={ () => this.toggleTodo(item) }/></td>
+            </tr>,
+        )
+
+    toggleTodo = (todo: TodoItem) =>
+        this.setState({
+            todoItems: this.state.todoItems.map(
+                item => item.action === todo.action
+                    ? { ...item, done: !item.done }
+                    : item,
+            ),
+        })
 
     render = () => (
         <div>
             <h4 className="bg-primary text-white text-center p-2">
                 { this.state.username }'s To-do List
-                ({ this.state.todoItems.filter(x => !x.done).length} items to do)
+                ({ this.state.todoItems.filter(x => !x.done).length } items to do)
             </h4>
             <div className="my-1">
                 <input className="form-control" value={ this.state.newItemText }
                        onChange={ e => this.setState({ newItemText: e.target.value }) }/>
                 <button className="btn btn-primary mt-1" onClick={ this.createNewTodo }>Add</button>
             </div>
+            <table className="table table-striped table-bordered">
+                <thead>
+                <tr>
+                    <th>Description</th>
+                    <th>Done</th>
+                </tr>
+                </thead>
+                <tbody>{ this.todoTableRows() }</tbody>
+            </table>
         </div>
     )
 }
